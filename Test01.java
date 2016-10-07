d0025a445fb6053ce8c8cef77d8f76c23303829b
VinhNT_Picachu <picachusays@gmail.com>
2016-10-08 02:32:09 +0700

    init 2016/10/8

---
+++ Test01.java
@@ -0,0 +1,17 @@
+import lib.JFrame_VinhNT;
+import test01.CuaSoTest01;
+import javax.swing.*;
+import javax.swing.plaf.metal.*;
+
+/**
+ * This sentence would hold the main description for this doc comment.
+ * @see java.lang.Object
+ */
+
+class Test01{
+    public static void main(String[] args){
+		JFrame_VinhNT.khoi_Tao_CuaSo();
+        CuaSoTest01 a = new CuaSoTest01();
+        System.out.print("xin chao cua so");
+    } 
+}
